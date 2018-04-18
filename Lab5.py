44ccabbb383b47bfda90ddb1490fd532cfa56e64
Tonio56 <anonio56reg@gmail.com>
2018-04-19 03:01:53 +0400

    Small improvements on Lab5

--- Lab5.py
+++ Lab5.py
@@ -15,25 +15,20 @@ img_HSV = np.zeros((ceil_w * ceil_ynum, ceil_w * ceil_xnum, 3), np.uint8)
 #   Strange pattern generation
 for x in range(ceil_xnum):
     for y in range(ceil_ynum):
-        bg_color_index = random.randint(0, len(color) - 1)
-        while True:
-            shape_color_index = random.randint(0, len(color) - 1)
-            if shape_color_index != bg_color_index:
-                break
-
+        bg_color, shape_color = random.sample(color, 2)
         shape_type = random.choice(("circle", "rect", "triangle"))
 
-        cv2.rectangle(img_GRY, (x * ceil_w, y * ceil_w), ((x + 1) * ceil_w, (y + 1) * ceil_w), (color[bg_color_index],), -1)
+        cv2.rectangle(img_GRY, (x * ceil_w, y * ceil_w), ((x + 1) * ceil_w, (y + 1) * ceil_w), bg_color, -1)
         if shape_type == "circle":
-            cv2.circle(img_GRY, (int((x + 0.5) * ceil_w), int((y + 0.5) * ceil_w)), int(ceil_w / 3), (color[shape_color_index],), -1)
+            cv2.circle(img_GRY, (int((x + 0.5) * ceil_w), int((y + 0.5) * ceil_w)), int(ceil_w / 3), shape_color, -1)
         elif shape_type == "rect":
-            cv2.rectangle(img_GRY, (int((x + 0.2) * ceil_w), int((y + 0.2) * ceil_w)), (int((x + 0.8) * ceil_w), int((y + 0.8) * ceil_w)), (color[shape_color_index],), -1)
+            cv2.rectangle(img_GRY, (int((x + 0.2) * ceil_w), int((y + 0.2) * ceil_w)), (int((x + 0.8) * ceil_w), int((y + 0.8) * ceil_w)), shape_color, -1)
         elif shape_type == "triangle":
             points = np.array([[int((x + 0.2) * ceil_w), int((y + 0.8) * ceil_w)],
                                [int((x + 0.8) * ceil_w), int((y + 0.8) * ceil_w)],
                                [int((x + 0.5) * ceil_w), int((y + 0.2) * ceil_w)]], np.int32)
             points = points.reshape((3, 1, 2))
-            cv2.fillPoly(img_GRY, [points], (color[shape_color_index],))
+            cv2.fillPoly(img_GRY, [points], shape_color)
 #   Calculation
 for i in range(img_GRY.shape[0]):
     for j in range(img_GRY.shape[1]):
@@ -41,8 +36,8 @@ for i in range(img_GRY.shape[0]):
                                         int(img_GRY[i - 1, j, 0])) / 2) ** 2 +
                                       ((int(img_GRY[i, (j + 1) % img_HSV.shape[1], 0]) -
                                         int(img_GRY[i, j - 1, 0])) / 2) ** 2)) / 255 * 180
-        img_HSV[i, j, 1] = (int(img_GRY[(i + 1) % img_HSV.shape[0], j, 0]) - int(img_GRY[i - 1, j, 0])) / 2
-        img_HSV[i, j, 2] = (int(img_GRY[i, (j + 1) % img_HSV.shape[1], 0]) - int(img_GRY[i, j - 1, 0])) / 2
+        img_HSV[i, j, 1] = math.fabs(int(img_GRY[(i + 1) % img_HSV.shape[0], j, 0]) - int(img_GRY[i - 1, j, 0])) // 2
+        img_HSV[i, j, 2] = math.fabs(int(img_GRY[i, (j + 1) % img_HSV.shape[1], 0]) - int(img_GRY[i, j - 1, 0])) // 2
 
 
 img_RGB = cv2.cvtColor(img_HSV, cv2.COLOR_HSV2BGR)
